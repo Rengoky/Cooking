83356f89292bce6b24dd787e06896ad8628c9861
Egor Kordiy <jormugran23@gmail.com>
2022-09-26 20:23:54 +0300

    create backend for foodgram

---
+++ backend/tags/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from . import models
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'slug'
+    )
---
+++ backend/users/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser, Follow
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('id', 'username', 'first_name', 'last_name',
+                    'email', 'password', 'is_staff', 'is_active',)
+    ordering = ('email',)
+    search_fields = ('username', 'email',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Follow)
